a87e0a2f4932b35333a6a41d9e38ac42820356d3
rajat <fzdsharmarajat@gmail.com>
2023-12-23 16:27:33 +0530

    weatherapp

---
+++ weatherapp/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Weatherapp from './components/weatherApp/weatherapp'
+
+function App() {
+  return (
+    
+      <BrowserRouter>
+      <Routes>
+       < Route path="/" element={<Weatherapp/>}/>
+
+      </Routes>
+      </BrowserRouter>
+     
+   
+  );
+}
+
+export default App;
+
+
+
---
+++ weatherapp/src/components/weatherApp/weatherapp.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./weatherapp.css";
+import searchicon from "../assets/searchicon.png";
+import cloudicon from "../assets/cloudimage.png";
+import humidityicon from "../assets/humidityicon.png";
+import windicon from "../assets/windicon.png";
+const Weatherapp = () => {
+  return (
+    <div className="container">
+      <div className="top-bar">
+        <input type="text" className="cityInput" placeholder="search" />
+        <div className="search-icon">
+          <img src={searchicon} alt="image" width="20px" />
+        </div>
+      </div>
+      <div className="cloud-icon">
+        <img src={cloudicon} alt="image" width="80px" />
+      </div>
+      <div className="weather-temp">24°C</div>
+      <div className="weather-loction">Jaipur</div>
+      <div className="data-container">
+        <div className="element">
+          <img src={humidityicon} width="60px" />
+          <div className="data">
+            <div className="humidity-percent">64%</div>
+            <div className="text">Humidity</div>
+          </div>
+        </div>
+        <div className="element">
+          <img src={windicon} width="60px" />
+          <div className="data">
+            <div className="humidity-percent">18 km/h</div>
+            <div className="text">Windspeed</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Weatherapp;
+
+
