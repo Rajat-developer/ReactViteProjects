4f7fdfe1e7f21528f93b882a9ce7ca31ee897b13
rajat <fzdsharmarajat@gmail.com>
2023-12-28 19:28:22 +0530

    learned more about nodeJs

--- forNodeJs/nodeJs/index.js
+++ forNodeJs/nodeJs/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -5,7 +6,10 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('hey rajat this side!');
 });
+app.get('/twitter', (req, res) => {
+    res.send('rajat.com!');
+  });
 
-app.listen(port, () => {
+app.listen(process.env.port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
