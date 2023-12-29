07ae58067e21c08f0de7243d0a89931f8a3d241f
rajat <fzdsharmarajat@gmail.com>
2023-12-30 01:08:36 +0530

    basic backend created

--- forNodeJs/nodeJs/index.js
+++ forNodeJs/nodeJs/index.js
@@ -1,15 +1,45 @@
-require('dotenv').config()
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+// import  express  from 'express';
 const app = express();
-const port = 3000;
+//const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('hey rajat this side!');
+// app.get("/", (req, res) => {
+//   res.send("server is ready");
+// });
+
+app.get("/api/products", (req, res) => {
+  const products = [
+    {
+      id: 1,
+      title: "For Face",
+      content: "Pears facewash",
+    },
+    {
+      id: 2,
+      title: "For Home",
+      content: "Easy portable Mop",
+    },
+    {
+      id: 3,
+      title: "For Car",
+      content: "Accessories",
+    },
+    {
+      id: 4,
+      title: "For Kitchen",
+      content: "Oven",
+    },
+    {
+      id: 5,
+      title: "For MobilePhones",
+      content: "BackCases, Tempered",
+    },
+  ];
+  res.send(products);
 });
-app.get('/twitter', (req, res) => {
-    res.send('rajat.com!');
-  });
 
+const port = process.env.PORT || 3000;
 app.listen(process.env.port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
